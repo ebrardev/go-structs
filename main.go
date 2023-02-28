08ec8b294c27801b583509598527888393b6466f
Ebrar <16155052@mersin.edu.tr>
2023-02-28 14:55:31 +0300

    structs

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
+	number    int
 }
 
 func main() {
+	alex := person{firstName: "Alex", lastName: "Anderson", number: 1423456789}
+	ebrar := person{firstName: "Ebrar", lastName: "mea", number: 123456789}
+	fmt.Println(alex, ebrar)
 
 }
