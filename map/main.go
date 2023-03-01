da78062d41b656659c4151ba41e5c841f411253d
Ebrar <16155052@mersin.edu.tr>
2023-03-01 10:02:27 +0300

    mapleme

--- map/main.go
+++ map/main.go
@@ -4,11 +4,17 @@ import "fmt"
 
 func main() {
 
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-		"blue":  "#0000ff",
-	}
+	// var colors map[string]string
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// 	"blue":  "#0000ff",
+	// }
+
+	colors := make(map[string]string)
+	colors["white"] = "#ffffff"
+	colors["black"] = "#000000"
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
